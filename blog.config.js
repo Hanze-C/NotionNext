cade37e726c3e464f26f1e86bddfa2ce46b26cd7
Hanze-C <hanze2010@outlook.com>
2025-06-07 17:58:51 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -62,10 +62,10 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，我是一个程序员, Hi，我是一个打工人,Hi，我是一个干饭人,欢迎来到我的博客🎉',
+    '长风破浪会有时, 直挂云帆济沧海, Hello World!',
 
   // uuid重定向至 slug
-  UUID_REDIRECT: process.env.UUID_REDIRECT || false
+  UUID_REDIRECT: process.env.UUID_REDIRECT || true
 }
 
 module.exports = BLOG
