b9adb785c5bfbd1dec184ddfbb2e9d95ebe0dadd
Hanze-C <hanze2010@outlook.com>
2025-06-07 19:17:56 +0800

    upd

--- themes/fukasawa/config.js
+++ themes/fukasawa/config.js
@@ -2,9 +2,9 @@ const CONFIG = {
   FUKASAWA_MAILCHIMP_FORM: false, // 邮件订阅表单
 
   FUKASAWA_POST_LIST_COVER: true, // 文章列表显示图片封面
-  FUKASAWA_POST_LIST_COVER_FORCE: false, // 即使没有封面也将站点背景图设置为封面
-  FUKASAWA_POST_LIST_PREVIEW: false, // 显示文章预览
-  FUKASAWA_POST_LIST_ANIMATION: false, // 博客列表淡入动画
+  FUKASAWA_POST_LIST_COVER_FORCE: true, // 即使没有封面也将站点背景图设置为封面
+  FUKASAWA_POST_LIST_PREVIEW: true, // 显示文章预览
+  FUKASAWA_POST_LIST_ANIMATION: true, // 博客列表淡入动画
 
   // 菜单
   FUKASAWA_MENU_CATEGORY: true, // 显示分类
@@ -13,6 +13,6 @@ const CONFIG = {
 
   FUKASAWA_SIDEBAR_COLLAPSE_BUTTON: true, // 侧边栏折叠按钮
   FUKASAWA_SIDEBAR_COLLAPSE_SATUS_DEFAULT: false, // 侧边栏默认折叠收起
-  FUKASAWA_SIDEBAR_COLLAPSE_ON_SCROLL: false // 侧边栏滚动时折叠 仅文章阅读页有效
+  FUKASAWA_SIDEBAR_COLLAPSE_ON_SCROLL: true // 侧边栏滚动时折叠 仅文章阅读页有效
 }
 export default CONFIG
